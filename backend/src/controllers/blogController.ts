cf78756432e3a5a4468c07007947a5305135ce2b
Anmol Kumar <kumaranmol8611@gmail.com>
2025-03-22 11:49:02 +0530

    updated blog order to latest first

--- backend/src/controllers/blogController.ts
+++ backend/src/controllers/blogController.ts
@@ -3,248 +3,247 @@ import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Context } from "hono";
 
-
 export const createBlog = async (c: Context) => {
-    const prisma = new PrismaClient({
-        datasourceUrl: c.env.DATABASE_URL,
-    }).$extends(withAccelerate());
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-    const body = await c.req.json();
-    const {success, data} = createBlogInput.safeParse(body);
-    if (!success) {
-        return c.json({
-          status: 403,
-          message: "Create Blog Input types are not correct",
-        });
+  const body = await c.req.json();
+  const { success, data } = createBlogInput.safeParse(body);
+  if (!success) {
+    return c.json({
+      status: 403,
+      message: "Create Blog Input types are not correct",
+    });
+  }
+  console.log(data);
+
+  const { title, content } = data;
+
+  const userId = await c.get("userId");
+
+  if (!title && !content) {
+    return c.json({
+      status: 400,
+      message: "Title, content and Author is required!",
+    });
+  }
+
+  try {
+    const newBlog = await prisma.post.create({
+      data: {
+        title,
+        content,
+        authorId: userId,
+      },
+    });
+
+    if (!newBlog) {
+      return c.json({
+        status: 500,
+        message: "Error creating blog!",
+      });
     }
-    console.log(data);
-    
-    const {title, content} = data;
 
-    const userId = await c.get('userId');
-
-    if(!title && !content) {
-        return c.json({
-            status : 400,
-            message : "Title, content and Author is required!"
-        })
+    return c.json({
+      status: 200,
+      message: "Blog posted successfully!",
+      data: newBlog,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return c.json({
+        status: 500,
+        message: error.message,
+      });
+    } else {
+      return c.json({
+        status: 500,
+        message: "Unknow error occured while creating blog",
+      });
     }
-
-    try {
-        const newBlog = await prisma.post.create({
-            data : {
-                title, content, authorId : userId
-            }
-        })
-
-        if(!newBlog) {
-            return c.json({
-                status : 500,
-                message : "Error creating blog!"
-            })
-        }
-
-        return c.json({
-            status : 200,
-            message : "Blog posted successfully!",
-            data : newBlog
-        })
-
-    } catch (error) {
-        if(error instanceof Error) {
-            return c.json({
-                status : 500,
-                message : error.message
-            })
-        } else {
-            return c.json({
-                status : 500,
-                message : "Unknow error occured while creating blog"
-            })
-        }
-    }
-}
+  }
+};
 
 export const updateBlog = async (c: Context) => {
-    const prisma = new PrismaClient({
-        datasourceUrl: c.env.DATABASE_URL,
-    }).$extends(withAccelerate());
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-    const body = await c.req.json();
+  const body = await c.req.json();
 
-    const {success, data} = updateBlogInput.safeParse(body);
-    if (!success) {
-        return c.json({
-          status: 403,
-          message: "update blog input types are not correct",
-        });
+  const { success, data } = updateBlogInput.safeParse(body);
+  if (!success) {
+    return c.json({
+      status: 403,
+      message: "update blog input types are not correct",
+    });
+  }
+  const { title, content, blogId } = data;
+
+  if (!title || !content) {
+    return c.json({
+      status: 400,
+      message: "Title or content is required!",
+    });
+  }
+
+  if (!blogId) {
+    return c.json({
+      status: 400,
+      message: "BlogId is required!",
+    });
+  }
+
+  try {
+    const updateBlog = await prisma.post.update({
+      where: {
+        id: blogId,
+      },
+      data: {
+        title,
+        content,
+        publishedAt: new Date(),
+      },
+    });
+
+    if (!updateBlog) {
+      return c.json({
+        status: 500,
+        message: "Error updating blog!",
+      });
     }
-    const {title, content , blogId} = data;
 
-    if(!title || !content) {
-        return c.json({
-            status : 400,
-            message : "Title or content is required!"
-        })
+    return c.json({
+      status: 200,
+      message: "Blog updated successfully!",
+      data: updateBlog,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return c.json({
+        status: 500,
+        message: error.message,
+      });
+    } else {
+      return c.json({
+        status: 500,
+        message: "Unknow error occured while updating blog",
+      });
     }
-
-    if(!blogId) {
-        return c.json({
-            status : 400,
-            message : "BlogId is required!"
-        })
-    }
-
-    try {
-        const updateBlog = await prisma.post.update({
-            where : {
-                id : blogId
-            },
-            data : {
-                title,
-                content,
-                publishedAt : new Date()
-            }
-        })
-
-        if(!updateBlog) {
-            return c.json({
-                status : 500,
-                message : "Error updating blog!"
-            })
-        }
-
-        return c.json({
-            status : 200,
-            message : "Blog updated successfully!",
-            data : updateBlog
-        })
-
-    } catch (error) {
-        if(error instanceof Error) {
-            return c.json({
-                status : 500,
-                message : error.message
-            })
-        } else {
-            return c.json({
-                status : 500,
-                message : "Unknow error occured while updating blog"
-            })
-        }
-    }
-}
+  }
+};
 
 export const getBlog = async (c: Context) => {
-    const prisma = new PrismaClient({
-        datasourceUrl: c.env.DATABASE_URL,
-    }).$extends(withAccelerate());
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-    const {id} = await c.req.param();
-    const blogId = id;
-    
+  const { id } = await c.req.param();
+  const blogId = id;
 
-    if(!blogId) {
-        return c.json({
-            status : 400,
-            message : "BlogId is required!"
-        })
+  if (!blogId) {
+    return c.json({
+      status: 400,
+      message: "BlogId is required!",
+    });
+  }
+
+  try {
+    const blog = await prisma.post.findUnique({
+      where: {
+        id: blogId,
+      },
+      select: {
+        id: true,
+        title: true,
+        content: true,
+        authorId: true,
+        author: {
+          select: {
+            name: true,
+          },
+        },
+        publishedAt: true,
+      },
+    });
+
+    if (!blog) {
+      return c.json({
+        status: 500,
+        message: "Error finding blog!",
+      });
     }
 
-    try {
-        const blog = await prisma.post.findUnique({
-            where : {
-                id : blogId
-            }, 
-            select : {
-                id : true,
-                title : true,
-                content : true,
-                authorId : true,
-                author : {
-                    select : {
-                        name : true,
-                    }
-                },
-                publishedAt : true
-            }
-        })
-
-        if(!blog) {
-            return c.json({
-                status : 500,
-                message : "Error finding blog!"
-            })
-        }
-
-        return c.json({
-            status : 200,
-            message : "Blog found successfully!",
-            data : blog
-        })
-
-    } catch (error) {
-        if(error instanceof Error) {
-            return c.json({
-                status : 500,
-                message : error.message
-            })
-        } else {
-            return c.json({
-                status : 500,
-                message : "Unknow error occured while finding blog"
-            })
-        }
+    return c.json({
+      status: 200,
+      message: "Blog found successfully!",
+      data: blog,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return c.json({
+        status: 500,
+        message: error.message,
+      });
+    } else {
+      return c.json({
+        status: 500,
+        message: "Unknow error occured while finding blog",
+      });
     }
-}
+  }
+};
 
 // add pagination
 
 export const getAllBlog = async (c: Context) => {
-    const prisma = new PrismaClient({
-        datasourceUrl: c.env.DATABASE_URL,
-    }).$extends(withAccelerate());
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-    try {
-        const blogs = await prisma.post.findMany({
-            select : {
-                id : true,
-                title : true,
-                content : true,
-                authorId : true,
-                author : {
-                    select : {
-                        name : true
-                    }
-                },
-                publishedAt : true
-            }
-        });
+  try {
+    const blogs = await prisma.post.findMany({
+      select: {
+        id: true,
+        title: true,
+        content: true,
+        authorId: true,
+        author: {
+          select: {
+            name: true,
+          },
+        },
+        publishedAt: true,
+      },
+      orderBy: {
+        publishedAt: "desc",
+      },
+    });
 
-        if(!blogs) {
-            return c.json({
-                status : 500,
-                message : "Error fetching blogs!"
-            })
-        }
-
-        return c.json({
-            status : 200,
-            message : "Blog fetched successfully!",
-            data : blogs
-        })
-
-    } catch (error) {
-        if(error instanceof Error) {
-            return c.json({
-                status : 500,
-                message : error.message
-            })
-        } else {
-            return c.json({
-                status : 500,
-                message : "Unknow error occured while fetching blog"
-            })
-        }
+    if (!blogs) {
+      return c.json({
+        status: 500,
+        message: "Error fetching blogs!",
+      });
     }
-}
+
+    return c.json({
+      status: 200,
+      message: "Blog fetched successfully!",
+      data: blogs,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return c.json({
+        status: 500,
+        message: error.message,
+      });
+    } else {
+      return c.json({
+        status: 500,
+        message: "Unknow error occured while fetching blog",
+      });
+    }
+  }
+};
--- backend/src/controllers/signInController.ts
+++ backend/src/controllers/signInController.ts
@@ -2,61 +2,59 @@ import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Context } from "hono";
 import { sign } from "hono/jwt";
-import {signinInput} from '@anmoldotx/writy-common'
+import { signinInput } from "@anmoldotx/writy-common";
 
-export const signInController = async (c : Context) => {
+export const signInController = async (c: Context) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-    const prisma = new PrismaClient({
-        datasourceUrl: c.env.DATABASE_URL,
-    }).$extends(withAccelerate());
+  const body = await c.req.json();
+  const { success, data } = signinInput.safeParse(body);
+  if (!success) {
+    return c.json({
+      status: 403,
+      message: "Signin Input types are not correct",
+    });
+  }
+  const { email, password } = data;
 
-    const body = await c.req.json();
-    const {success, data} = signinInput.safeParse(body);
-    if (!success) {
-        return c.json({
-          status: 403,
-          message: "Signin Input types are not correct",
-        });
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return c.json({
+        status: 401,
+        message: "User does not exist!",
+      });
     }
-    const {email, password} = data;
 
-    try {
-        const user = await prisma.user.findUnique({
-            where : {email}
-        })
-
-        if(!user) {
-            return c.json({
-                status : 401,
-                message : "User does not exist!"
-            })
-        }
-
-        if(user?.password !== password) {
-            
-            return c.json({
-                status : 403,
-                message : "Invalid Password!"
-            })
-        }
-    
-        const jwt = await sign({id : user?.id}, c.env.SIGNIN_SECRET);
-        return c.json({
-            status : 200,
-            message : "User logged in successfully!",
-            token : jwt
-        })
-    } catch (error) {
-        if(error instanceof Error) {
-            return c.json({
-                status : 500,
-                message : error.message
-            })
-        } else {
-            return c.json({
-                status : 500,
-                message : "Unknow error while sign-in"
-            })
-        }
+    if (user?.password !== password) {
+      return c.json({
+        status: 403,
+        message: "Invalid Password!",
+      });
     }
-}
+
+    const jwt = await sign({ id: user?.id }, c.env.SIGNIN_SECRET);
+    return c.json({
+      status: 200,
+      message: "User logged in successfully!",
+      token: jwt,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return c.json({
+        status: 500,
+        message: error.message,
+      });
+    } else {
+      return c.json({
+        status: 500,
+        message: "Unknow error while sign-in",
+      });
+    }
+  }
+};
